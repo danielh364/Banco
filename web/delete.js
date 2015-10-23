7873736b60f9d1a4857d629563d68b70858ae877
danie <danie@Daniel-PC>
2015-10-23 22:15:00 +0200

    commit

--- web/delete.js
+++ web/delete.js
@@ -1,7 +1,9 @@
 app.controller("deleteController", deleteController);
 deleteController.Sinject=['$scope','$http','$routeParams'];
 function deleteController($scope, $http, $routeParams) {
-   
+      $scope.identidadBancaria=$routeParams.idEntidadBancaria
+   $scope.borrar = function () { 
+
     var config = {
         method: "DELETE",
         url: "../Banco.api/api/entidadbancaria/"+$routeParams.idEntidadBancaria
@@ -11,5 +13,16 @@ function deleteController($scope, $http, $routeParams) {
         $scope.entidadBancaria = data;
            alert("se ha borrado Correctamente");
     });
+   }
+   
+   $scope.noBorrar = function () { 
+   
+    var config = {} 
+    
+    var response = $http(config);
+    response.success(function (data, status, headers, config) {
+        $scope.entidadBancaria = data;
+           alert("No se ha borrado la Entidad Bancaria");
+    });
 }
-
+}
